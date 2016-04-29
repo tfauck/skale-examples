6e0ad888d30b5c11b7f9f4dc5281f3d443263273
Thierry Fauck <tfauck@free.fr>
2016-04-29 04:45:14 -0400

    Add new examples pi and adults 	new file:   adult/.gitignore 	new file:   adult/adult.data 	new file:   adult/adult.js 	new file:   adult/adult.test 	new file:   adult/package.json 	new file:   adult/roc.png 	new file:   pi/package.json 	new file:   pi/pi.js

---
+++ pi/pi.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var sc = require('skale-engine').context();
+
+var NUM_SAMPLES = 1000000;
+
+function sample() {
+	var x = Math.random(), y = Math.random();
+	return ((x * x + y * y) < 1) ? 1 : 0;
+}
+
+sc.range(0, NUM_SAMPLES).map(sample).reduce((a, b) => a + b, 0).on('data', function(count) {
+	console.log('Pi is roughly ', (4.0 * count / NUM_SAMPLES));
+}).on('end', sc.end);
